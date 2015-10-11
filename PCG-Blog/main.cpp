b092dfcc75ac768a78658402fe5f2a837ccd52a8
CMilby <cmilby99@gmail.com>
2015-10-11 19:13:27 -0400

    Initial Commit

---
+++ PCG-Blog/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PCG-Blog
+//
+//  Created by Craig Milby on 10/11/15.
+//  Copyright © 2015 Craig Milby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
